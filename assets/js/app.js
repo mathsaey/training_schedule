71eef50be09913f4800a6470f519a75507df0bff
Mathijs Saey <mathijs@mathsaey.be>
2023-03-27 22:14:29 +0200

    move drag and drop to module

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,12 @@ import WorkoutDragAndDropHooks from "./workout-drag-drop"
 
 let Hooks = {}
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {_csrf_token: csrfToken},
+  hooks: {
+    WorkoutDragAndDrop: WorkoutDragAndDropHooks
+  }
+})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
@@ -22,61 +27,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
-Hooks.WorkoutDragAndDrop = {
-  findDropableParent(el) {
-    while (!el.attributes.dropable) {
-      el = el.parentElement
-    }
-    return el
-   },
-
-  mounted() {
-    const dropClasses = ["border-sky-500", "dark:border-sky-500"]
-    const loadingClass = "opacity-50"
-
-    this.el.addEventListener("dragstart", (e) => {
-      let workoutId = e.target.id
-      let source = this.findDropableParent(e.target)
-      let data = JSON.stringify({'workout': workoutId, 'source': source.id})
-      e.dataTransfer.setData("text/plain", data)
-      e.dataTransfer.effectAllowed = "copyMove"
-    })
-
-    this.el.addEventListener("dragenter", e => {
-      e.preventDefault()
-      this.findDropableParent(e.target).classList.add(...dropClasses)
-    })
-
-    this.el.addEventListener("dragleave", e => {
-      e.preventDefault()
-      this.findDropableParent(e.target).classList.remove(...dropClasses)
-    })
-
-    this.el.addEventListener("drop", e => {
-      e.preventDefault()
-      let data = JSON.parse(e.dataTransfer.getData("text/plain"))
-      let dest = this.findDropableParent(e.target)
-      if (dest.id == data.source) { return }
-
-      let workout = e.view.document.getElementById(data.workout)
-      this.pushEvent("workout_dragged", {
-        "action": e.dataTransfer.dropEffect,
-        "workout": data.workout,
-        "target": dest.id,
-      })
-
-      dest.classList.remove(...dropClasses)
-
-      if (e.dataTransfer.dropEffect == "move") {
-        workout.classList.add(loadingClass)
-        dest.appendChild(workout)
-      } else {
-        let copy = workout.cloneNode(true)
-        copy.id = `${copy.id}_copy_${dest.id}`
-        copy.classList.add(loadingClass)
-        dest.appendChild(copy)
-      }
-    })
-  }
-}
---
+++ assets/js/workout-drag-drop.js
@@ -0,0 +1,58 @@
+export default {
+  findDropableParent(el) {
+    while (!el.attributes.dropable) {
+      el = el.parentElement
+    }
+    return el
+   },
+
+  mounted() {
+    const dropClasses = ["border-sky-500", "dark:border-sky-500"]
+    const loadingClass = "opacity-50"
+
+    this.el.addEventListener("dragstart", (e) => {
+      let workoutId = e.target.id
+      let source = this.findDropableParent(e.target)
+      let data = JSON.stringify({'workout': workoutId, 'source': source.id})
+      e.dataTransfer.setData("text/plain", data)
+      e.dataTransfer.effectAllowed = "copyMove"
+    })
+
+    this.el.addEventListener("dragenter", e => {
+      e.preventDefault()
+      this.findDropableParent(e.target).classList.add(...dropClasses)
+    })
+
+    this.el.addEventListener("dragleave", e => {
+      e.preventDefault()
+      this.findDropableParent(e.target).classList.remove(...dropClasses)
+    })
+
+    this.el.addEventListener("drop", e => {
+      e.preventDefault()
+      let data = JSON.parse(e.dataTransfer.getData("text/plain"))
+      let dest = this.findDropableParent(e.target)
+      if (dest.id == data.source) { return }
+
+      let workout = e.view.document.getElementById(data.workout)
+      this.pushEvent("workout_dragged", {
+        "action": e.dataTransfer.dropEffect,
+        "workout": data.workout,
+        "target": dest.id,
+      })
+
+      dest.classList.remove(...dropClasses)
+
+      if (e.dataTransfer.dropEffect == "move") {
+        workout.classList.add(loadingClass)
+        dest.appendChild(workout)
+      } else {
+        let copy = workout.cloneNode(true)
+        copy.id = `${copy.id}_copy_${dest.id}`
+        copy.setAttribute("draggable", false)
+        copy.classList.add(loadingClass)
+        dest.appendChild(copy)
+      }
+    })
+  }
+}
